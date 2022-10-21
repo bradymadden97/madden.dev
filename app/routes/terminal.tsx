67337e3adb71d08104a3bf7b5adafc46a6edbe98
Brady Madden <bradymadden97@gmail.com>
2022-10-21 19:36:39 +0100

    terminal

--- app/routes/terminal.tsx
+++ app/routes/terminal.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import styles from "~/styles/terminal.css";
 import lexicalStyles from "~/styles/terminal-lexical.css";
+
+import { mergeRegister } from "@lexical/utils";
 import {
   $createParagraphNode,
   $createRangeSelection,
@@ -15,16 +17,20 @@ import {
   EditorConfig,
   EditorState,
   ElementNode,
+  KEY_ARROW_DOWN_COMMAND,
   KEY_ARROW_LEFT_COMMAND,
   KEY_ARROW_RIGHT_COMMAND,
+  KEY_ARROW_UP_COMMAND,
   KEY_ENTER_COMMAND,
   KEY_MODIFIER_COMMAND,
   LexicalEditor,
   MOVE_TO_START,
   NodeKey,
+  NodeSelection,
   ParagraphNode,
   RangeSelection,
   SELECTION_CHANGE_COMMAND,
+  SerializedEditorState,
   SerializedTextNode,
   Spread,
   TextNode,
@@ -39,6 +45,8 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { parseEditorState } from "lexical/LexicalUpdates";
+import { cloneEditorState } from "lexical/LexicalEditorState";
 
 export function links() {
   return [
@@ -57,28 +65,7 @@ export function links() {
   ];
 }
 
-const DEFAULT_START = "~ $ ";
-
 export default function Index() {
-  const [userInput, setUserInput] = useState(DEFAULT_START);
-  const [cursorPosition, setCursorPosition] = useState(userInput.length);
-
-  const initialConfig = {
-    namespace: "MyEditor",
-    theme: {
-      base: "terminal-base",
-    },
-    onError: (e: Error) => console.error(e),
-    editorState: () => {
-      const paragraph = $createParagraphNode();
-      paragraph.append(new TerminalPrefixNode());
-      $getRoot().append(paragraph);
-      $getRoot().selectEnd();
-    },
-    nodes: [TerminalPrefixNode],
-    editable: true,
-  };
-
   return (
     <div id="term-container">
       <div id="term-border">
@@ -100,40 +87,178 @@ export default function Index() {
             }}
             value={userInput + "\u2588"}
           />*/}
-          <LexicalComposer initialConfig={initialConfig}>
-            <PlainTextPlugin
-              contentEditable={
-                <ContentEditable
-                  autoCorrect={false}
-                  // autoComplete={false}
-                  autoCapitalize={false}
-                  spellCheck={false}
-                  id="term-body-content-editable"
-                />
-              }
-              placeholder=""
-            />
-            <OnChangePlugin onChange={onChange} />
-            <HistoryPlugin />
-            <TransformPlugin />
-          </LexicalComposer>
+          <ActiveComposer />
         </div>
       </div>
     </div>
   );
 }
 
-function onChange(editorState: EditorState, editor: LexicalEditor) {
-  editorState.read(() => {
-    // Read the contents of the EditorState here.
-    const root = $getRoot();
-    const selection = $getSelection();
+function ActiveComposer() {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme: {
+      base: "terminal-base",
+    },
+    onError: (e: Error) => console.error(e),
+    editorState: () => {
+      const paragraph = $createParagraphNode();
+      paragraph.append(new TerminalPrefixNode());
+      $getRoot().append(paragraph);
+      $getRoot().selectEnd();
+    },
+    nodes: [TerminalPrefixNode],
+    editable: true,
+  };
 
-    // console.log(selection);
-  });
+  const [commandHistory, setCommandHistory] = useState<
+    Array<{ editorState: EditorState; timeStamp: number }>
+  >([]);
+  const [commandPointer, setCommandPointer] = useState(0);
+  const [lastClearedCommandIndex, setLastClearedCommandIndex] = useState<
+    number | undefined
+  >(undefined);
+
+  const onEnter = (newCommand: EditorState) => {
+    storeCommandToHistory(newCommand);
+    executeCommand(newCommand);
+  };
+
+  const onCtrlC = (newCommand: EditorState) => {
+    storeCommandToHistory(newCommand);
+  };
+
+  const storeCommandToHistory = (newCommand: EditorState) => {
+    setCommandPointer(0);
+    setCommandHistory((prev) => {
+      const endSelection = $createRangeSelection();
+      const end = $getRoot().getLastDescendant();
+      if (end != null) {
+        endSelection.anchor.set(end.getKey(), end.getTextContentSize(), "text");
+        endSelection.focus.set(end.getKey(), end.getTextContentSize(), "text");
+      }
+      return [
+        ...prev,
+        {
+          editorState: newCommand.clone(endSelection),
+          timeStamp: Date.now(),
+        },
+      ];
+    });
+  };
+
+  const executeCommand = (newCommand: EditorState) => {
+    newCommand.read(() => {
+      // TODO: Going to need editor to be passed in here so we can update
+
+      /**
+       * Clean up command and strip prefix characters
+       */
+      let command = $getRoot().getTextContent();
+      const terminalPrefix = $getRoot().getFirstDescendant();
+      if (terminalPrefix instanceof TerminalPrefixNode) {
+        if (command.startsWith(terminalPrefix.getTextContent())) {
+          command = command.substring(terminalPrefix.getTextContentSize());
+        }
+      }
+      command = command.trim();
+
+      /**
+       * Match and dispatch the commands
+       */
+      switch (command) {
+        case "clear":
+          setLastClearedCommandIndex(commandHistory.length);
+          break;
+
+        default:
+          console.log("Unhandled: ", command);
+          break;
+      }
+    });
+  };
+
+  const onGetCommandHistory = useCallback(
+    (history: "prev" | "next") => {
+      const lastIndex = commandHistory.length;
+      let newPointer;
+      switch (history) {
+        case "prev":
+          newPointer = Math.min(lastIndex, commandPointer + 1);
+          break;
+        case "next":
+          newPointer = Math.max(0, commandPointer - 1);
+          break;
+      }
+
+      setCommandPointer(newPointer);
+
+      if (newPointer === 0) {
+        return null;
+      } else {
+        const atPointer = commandHistory[lastIndex - newPointer];
+        return { ...atPointer, editorState: atPointer?.editorState?.clone() };
+      }
+    },
+    [commandHistory, commandPointer]
+  );
+
+  return (
+    <>
+      {commandHistory
+        .slice(
+          lastClearedCommandIndex != undefined
+            ? lastClearedCommandIndex + 1
+            : undefined
+        )
+        .map((history) => (
+          <LexicalComposer
+            key={history.timeStamp}
+            initialConfig={{
+              ...initialConfig,
+              editable: false,
+              editorState: history.editorState,
+            }}
+          >
+            <PlainTextPlugin
+              contentEditable={
+                <ContentEditable
+                  readOnly={true}
+                  className="term-body-content-editable"
+                />
+              }
+              placeholder=""
+            />
+          </LexicalComposer>
+        ))}
+      <LexicalComposer initialConfig={initialConfig}>
+        <PlainTextPlugin
+          contentEditable={
+            <ContentEditable
+              autoCorrect={false}
+              // autoComplete={false}
+              autoCapitalize={false}
+              spellCheck={false}
+              className="term-body-content-editable"
+            />
+          }
+          placeholder=""
+        />
+
+        <HistoryPlugin />
+
+        <PreservePrefixPlugin />
+        <TerminalKeyCommandsPlugin
+          onCtrlC={onCtrlC}
+          onEnter={onEnter}
+          onGetCommandHistory={onGetCommandHistory}
+        />
+      </LexicalComposer>
+    </>
+  );
 }
 
-function TransformPlugin(): null {
+function PreservePrefixPlugin(): null {
   const [editor] = useLexicalComposerContext();
 
   editor.registerNodeTransform(ParagraphNode, (paragraphNode) => {
@@ -194,26 +319,52 @@ function TransformPlugin(): null {
   );
 
   editor.registerCommand<KeyboardEvent>(
-    KEY_MODIFIER_COMMAND,
+    KEY_ARROW_RIGHT_COMMAND,
     (event: KeyboardEvent, editor: LexicalEditor) => {
-      if (event.ctrlKey && event.key === "c") {
-        const paragraph = $createParagraphNode();
-        paragraph.append(new TerminalPrefixNode());
-        $getRoot().clear();
-        $getRoot().append(paragraph);
-        $getRoot().selectEnd();
+      // If we're holding shift, we're highlighting
+      if (event.shiftKey) {
+        return false;
+      }
 
-        event.preventDefault();
-        return true;
+      // If not RangeSelection, bail early
+      const selection = $getSelection();
+      if (!$isRangeSelection(selection)) {
+        return false;
+      }
+
+      // If any part of the selection contains TerminalPrefixNode
+      // and the end of the offset is not the end of the TerminalPrefixNode,
+      //  set the cursor to the end of TerminalPrefixNode
+      const selectionNodes = selection.getNodes();
+      if (!selectionNodes.every((n) => !(n instanceof TerminalPrefixNode))) {
+        const offsets = selection.getCharacterOffsets();
+        const root = $getRoot();
+        const terminalPrefix = root.getFirstDescendant() as TerminalPrefixNode;
+        if (offsets[1] < terminalPrefix.getLength()) {
+          const newSelection = $createRangeSelection();
+          newSelection.anchor.set(
+            terminalPrefix.getKey(),
+            terminalPrefix.getLength(),
+            "text"
+          );
+          newSelection.focus.set(
+            terminalPrefix.getKey(),
+            terminalPrefix.getLength(),
+            "text"
+          );
+          $setSelection(newSelection);
+          event.preventDefault();
+          return true;
+        }
       }
       return false;
     },
     COMMAND_PRIORITY_EDITOR
   );
 
-  editor.registerCommand<void>(
-    SELECTION_CHANGE_COMMAND,
-    (_event: void, _editor: LexicalEditor) => {
+  editor.registerCommand<MouseEvent>(
+    CLICK_COMMAND,
+    (event: MouseEvent, editor: LexicalEditor) => {
       const selectionNodes = $getSelection()?.getNodes();
       if (selectionNodes == null) {
         return false;
@@ -221,7 +372,6 @@ function TransformPlugin(): null {
 
       const selection = $getSelection();
       if (!$isRangeSelection(selection)) {
-        console.log(selection);
         return false;
       }
 
@@ -231,8 +381,6 @@ function TransformPlugin(): null {
         const onlyNode = selectionNodes[0];
         if (onlyNode instanceof TerminalPrefixNode) {
           if (offsets[0] !== onlyNode.getLength()) {
-            console.log(selection.focus.offset, selection.anchor.offset);
-            // console.log(offsets);
             const newSelection = $createRangeSelection();
             newSelection.anchor.set(
               onlyNode.getKey(),
@@ -244,7 +392,8 @@ function TransformPlugin(): null {
               onlyNode.getLength(),
               "text"
             );
-            //$setSelection(newSelection);
+            $setSelection(newSelection);
+            event.preventDefault();
             return true;
           }
         }
@@ -284,20 +433,72 @@ function TransformPlugin(): null {
     COMMAND_PRIORITY_EDITOR
   );
 
-  editor.registerCommand<KeyboardEvent>(
-    KEY_ENTER_COMMAND,
-    (event: KeyboardEvent, editor: LexicalEditor) => {
-      const paragraph = $createParagraphNode();
-      paragraph.append(new TerminalPrefixNode());
-      $getRoot().clear();
-      $getRoot().append(paragraph);
-      $getRoot().selectEnd();
+  return null;
+}
 
-      event.preventDefault();
-      return true;
-    },
-    COMMAND_PRIORITY_EDITOR
-  );
+function TerminalKeyCommandsPlugin(props: {
+  onCtrlC: (command: EditorState) => void;
+  onEnter: (command: EditorState) => void;
+  onGetCommandHistory: (history: "prev" | "next") => {
+    editorState: EditorState;
+    timeStamp: number;
+  } | null;
+}): null {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    return mergeRegister(
+      editor.registerCommand<KeyboardEvent>(
+        KEY_ARROW_UP_COMMAND,
+        (event: KeyboardEvent, editor: LexicalEditor) => {
+          const history = props.onGetCommandHistory("prev");
+          if (history != null) {
+            editor.setEditorState(history.editorState);
+          }
+          event.preventDefault();
+          return true;
+        },
+        COMMAND_PRIORITY_EDITOR
+      ),
+      editor.registerCommand<KeyboardEvent>(
+        KEY_ARROW_DOWN_COMMAND,
+        (event: KeyboardEvent, editor: LexicalEditor) => {
+          const history = props.onGetCommandHistory("next");
+          if (history != null) {
+            editor.setEditorState(history.editorState);
+          } else {
+            $clearEntry();
+          }
+          event.preventDefault();
+          return true;
+        },
+        COMMAND_PRIORITY_EDITOR
+      ),
+      editor.registerCommand(
+        KEY_MODIFIER_COMMAND,
+        (event, editor) => {
+          if (event.ctrlKey && event.key === "c") {
+            props.onCtrlC(editor.getEditorState());
+            $clearEntry();
+            event.preventDefault();
+            return true;
+          }
+          return false;
+        },
+        COMMAND_PRIORITY_EDITOR
+      ),
+      editor.registerCommand<KeyboardEvent>(
+        KEY_ENTER_COMMAND,
+        (event: KeyboardEvent, editor: LexicalEditor) => {
+          props.onEnter(editor.getEditorState());
+          $clearEntry();
+          event.preventDefault();
+          return true;
+        },
+        COMMAND_PRIORITY_CRITICAL /* COMMAND_PRIORITY_EDITOR not working for some reason */
+      )
+    );
+  }, [editor, props.onGetCommandHistory, props.onEnter, props.onCtrlC]);
 
   return null;
 }
@@ -360,3 +561,19 @@ class TerminalPrefixNode extends TextNode {
 export function $createTerminalPrefixNode(): TerminalPrefixNode {
   return new TerminalPrefixNode();
 }
+
+export function $clearEntry(): void {
+  const paragraph = $createParagraphNode();
+  paragraph.append(new TerminalPrefixNode());
+  $getRoot().clear();
+  $getRoot().append(paragraph);
+  $getRoot().selectEnd();
+}
+
+/**
+ * TODO
+ * [] Handle commands
+ * [] Use block cursor
+ * [] Autofocus when clicking anywhere within terminal
+ *
+ */
